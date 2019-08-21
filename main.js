5969538ccc2a82ebb80e17a3d0247e95900d2e94
Jon Lugg <jon.lugg@Nology.local>
2019-08-21 09:15:48 +0100

    update

--- main.js
+++ main.js
@@ -1,4 +1,69 @@
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
+const engToMorse = document.getElementById("engToMorse");
+const morseToEng = document.getElementById("morseToEng");
+const inputEnglish = document.getElementById("inputEnglish");
+const inputMorse = document.getElementById("inputMorse");
+
+const morseKey = {
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-",
+  " ": "/"
+};
+
+const letterToMorse = letter => {
+  return morseKey[letter];
+};
+
+console.log(letterToMorse("@"));
+
+engToMorse.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log(inputEnglish.value);
+});
+
+morseToEng.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log(inputMorse.value);
+});
--- main.test.js
+++ main.test.js
@@ -1,5 +1,15 @@
-const sum = require("./main");
+let translate;
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("English to Morse code", () => {
+  expect(translate("a")).toBe(".-");
+  expect(translate("z")).toBe("--..");
+  expect(translate("barry")).toBe("-... .- .-. .-. -.--");
+});
+
+test("Morse code to English", () => {
+  expect(translate(".-")).toBe("a");
+  expect(translate("--..")).toBe("z");
+  expect(translate("-... .- .-. .-. -.-- / .-.. .- .-. .-. -.--")).toBe(
+    "barry larry"
+  );
 });
